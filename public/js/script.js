7831a722b85fef068af7aabd8d5d1403fcc172a3
DESKTOP-7M01SED\Aluno <aluno@jaboatao.ifpe.edu.br>
2023-01-18 11:48:38 -0300

    edit ejs e script

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,37 @@
 
 // Validar campos de login de Alunos e Professores
 const numMat = document.querySelector('#numero_matricula');
-const senha = document.querySelector('#senha_aluno');
+const senha = document.querySelector('#senha');
 const submitErro = document.querySelector('.erro_');
 const prof = document.querySelector('#prof');
 const aluno = document.querySelector('#aluno');
+
+// prof.addEventListener('submit', e => {
+//     e.preventDefault();
+//     if(validar()) {
+//         let dadoForm = ler();
+//         if(selectedRow == null) {
+//             inserirNovo(dadoForm);
+//         } else {
+//             atualizar(dadoForm)
+//         }
+//         resetForm();
+//     }
+// })
+
+
+function validar() {
+    isValid = true;
+    if(numMat.value == '' || senha.value == '') {
+        isValid = false;
+        submitErro.style.display == 'block';
+    } else {
+        isValid = true;
+        submitErro.style.display == 'none';
+    }
+    return isValid;
+}
+
 
 // Menu modal
 const btnNovo = document.querySelector('#btn-add');
@@ -22,23 +49,47 @@ btnCancelar.addEventListener('click', fecharModal => {
     mostrarConteudoModal.style.display = 'none'
 })
 
-// function abrirModal() {
-    
-// }
-
-
-
-//
-
-
-
 // Codigo do CRUD
-//const modal = document.querySelector('.modal-container');
+
 const tbody = document.querySelector('tbody');
 const sMatAluno = document.querySelector('#mat_aluno');
 const sNomeAluno = document.querySelector('#nome_aluno');
 const sIdadeAluno = document.querySelector('#idade_aluno');
 const sTelAluno = document.querySelector('#tel_aluno');
 
+function ler() {
+    let dadoForm = {};
+    dadoForm['num_matricula'] = document.querySelector('#mat_aluno').value;
+    dadoForm['nomeAluno'] = document.querySelector('#nome_aluno').value;
+    dadoForm['idadeAluno'] = document.querySelector('#idade_aluno').value;
+    dadoForm['telAluno'] = document.querySelector('#tel_aluno').value;
+    dadoForm['statusAluno'] = document.querySelector('#status_aluno').value;
+}
+
+function inserirNovo(dado) {
+    let tabela = documente.querySelector('#listaAlunos').getElementsByTagName('tbody')[0];
+    let novaLinha = tabela.insertRow(tabela.length);
+    celula1 = novaLinha.insertCell(0);
+    celula1.innerHTML = dado.num_matricula;
+    celula2 = novaLinha.insertCell(1);
+    celula2.innerHTML = dado.nomeAluno;
+    celula3 = novaLinha.insertCell(2);
+    celula3.innerHTML = dado.idadeAluno;
+    celula4 = novaLinha.insertCell(3);
+    celula4.innerHTML = dado.telAluno;
+    celula5 = novaLinha.insertCell(4);
+    celula5.innerHTML = dado.statusAluno;
+    celula6.innerHTML = `<a >Editar</a>`
+                        `<a ${deletar(this)}>Apagar</a>`
+}
+
+function resetForm() {
+    document.querySelector('#mat_aluno').value;
+    document.querySelector('#nome_aluno').value;
+    document.querySelector('#idade_aluno').value;
+    document.querySelector('#tel_aluno').value;
+    document.querySelector('#status_aluno').value;
+    selectedRow = null;
+}
 
 
